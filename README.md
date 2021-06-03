98f4a436c722af6517bfef2a872a4dc2ba56f878
Brian Blaylock <blaylockbk@gmail.com>
2021-06-03 16:54:44 +0000

    new logo

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
+![](docs/_static/HerbieLogo2_transparent.png)
+
 # 📔 [Documentation Page](https://blaylockbk.github.io/HRRR_archive_download/_build/html/)
 
 |![](https://raw.githubusercontent.com/blaylockbk/SynopticPy/master/images/Balloon_logo/balloon_bkb_sm.png)|**Brian Blaylock**<br>🌐 [Webpage](http://home.chpc.utah.edu/~u0553130/Brian_Blaylock/home.html)<br>[🔩 PyPI](https://pypi.org/user/blaylockbk/)
 |:--|:--|
-
-
 
 # Brian's High-Resolution Rapid Refresh tools: HRRR-B
 
--- herbie/misc.py
+++ herbie/misc.py
@@ -6,27 +6,54 @@ import matplotlib.pyplot as plt
 def HerbieColors():
     return dict(body='#f0ead2', red='#88211b', blue='#0c3576', white='#ffffff', black='#000000')
 
-def HerbieLogo():
+def HerbieLogo(white_line=False):
     """Logo of Herbie The Love Bug"""
-    colors = dict(
-        body='#f0ead2', 
-        red='#88211b', 
-        blue='#0c3576', 
-        white='#ffffff', 
-        black='#000000'
-    )
+    colors = HerbieColors()
 
     plt.figure(figsize=[5,5], facecolor=colors['body'])
 
     plt.axis([-10, 10, -10, 10])
 
+    if white_line:
+        plt.axvline(4, lw=40, color=colors['white'])
     plt.axvline(2.5, lw=20, color=colors['red'])
     plt.axvline(5.5, lw=40, color=colors['blue'])
+    
 
     c=plt.Circle((0, 0), radius=6, ec='k', fc='w', zorder=10, linewidth=3)
     plt.gca().add_artist(c)
 
     plt.text(0,0, '53', fontsize=110, fontweight='bold', va='center_baseline', ha='center', zorder=11)
+
+    plt.gca().set_facecolor(colors['body'])
+    plt.gca().margins(0)
+
+    plt.gca().get_xaxis().set_visible(False)
+    plt.gca().get_yaxis().set_visible(False)
+
+    plt.gca().spines['bottom'].set_visible(False)
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+
+    return plt.gca()
+
+def HerbieLogo2(white_line=False):
+    """Logo of Herbie The Love Bug"""
+    colors = HerbieColors()
+
+    plt.figure(figsize=[5,3], facecolor=colors['body'])
+
+    plt.axis([1.5, 20, -10, 10])
+
+    if white_line:
+        plt.axvline(4, lw=40, color=colors['white'])
+    plt.axvline(2.5, lw=20, color=colors['red'])
+    plt.axvline(5.5, lw=40, color=colors['blue'])
+
+    
+
+    plt.text(8,0, 'Herbie', fontsize=110, fontweight='bold', va='center_baseline', ha='left', zorder=11)
 
     plt.gca().set_facecolor(colors['body'])
     plt.gca().margins(0)
