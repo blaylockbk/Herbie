9bb2c18f8a5b71122acb93ca75746fa372a97f81
Brian Blaylock <blaylockbk@users.noreply.github.com>
2021-10-01 19:20:18 -0700

    first time writing a test

---
+++ tests/test_first.py
@@ -0,0 +1,12 @@
+## Brian Blaylock
+## October 1, 2021
+
+"""
+I have no idea what I'm doing yet. First time making a test.
+"""
+
+import numpy as np
+
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
