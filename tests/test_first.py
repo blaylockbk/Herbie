adda592b7b67cd12f1354cf45d17cb4c84fa7d57
Brian Blaylock <blaylockbk@gmail.com>
2021-10-05 16:22:01 +0000

    hey look, I ran a simple test

--- tests/test_first.py
+++ tests/test_first.py
@@ -4,9 +4,14 @@
 """
 I have no idea what I'm doing yet. First time making a test.
 """
-
 import numpy as np
 
 def test_sqrt():
    num = 25
-   assert math.sqrt(num) == 5
+   assert np.sqrt(num) == 5
+
+def test_str():
+   a = "Herbie"
+   b = "Herbie"
+   assert a==b
+
