2c0a3980047eba40a3e47d49620d26044234c84d
Brian Blaylock <blaylockbk@users.noreply.github.com>
2021-11-19 10:32:11 -0800

    minor changes

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = "sphinx_rtd_theme"
 html_theme = "pydata_sphinx_theme"
 
 html_theme_options = {
