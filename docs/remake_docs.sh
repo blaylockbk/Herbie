39b8bc97c770995a35ba53a226c2349f083fa0d0
Brian Blaylock <blaylockbk@gmail.com>
2021-12-16 19:47:14 +0000

    updates

--- docs/remake_docs.sh
+++ docs/remake_docs.sh
@@ -10,18 +10,11 @@ conda activate herbie
 git add -A
 git commit -m "updates"
 
-## Make Sphinx documents for pyBKB_NRL
-
-# Load the conda environment
-#conda activate basic
-#conda info
-
 # Make the html docs
 make html
 
 # Zip it up for easy download to local computer
 cd _build
-#zip -q -r html.zip html
 
 # Add these changes to GitHub
 git add -A
