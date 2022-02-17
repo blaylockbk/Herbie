817ad0b58eb135f7b1ae4a8ef399fc5ec5ce964a
blaylockbk <blaylockbk@gmail.com>
2022-02-17 02:10:45 +0000

    update

--- herbie/models/__init__.py
+++ herbie/models/__init__.py
@@ -11,10 +11,15 @@ from .ecmwf import *
 try:
     # An experimental special case for locally stored files.
     from .local import *
+
+    print("Template for [local] was loaded.")
 except:
     pass
+
 try:
     # Brian's personal local special case (hidden).
     from .local_B import *
+
+    print("Template from [local_B] was loaded.")
 except:
     pass
