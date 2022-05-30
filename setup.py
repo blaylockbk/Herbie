3c11e01ab59eb3f1ea139a11bdc26dd5888b0379
Brian Blaylock <blaylockbk@gmail.com>
2022-05-30 14:49:39 -0700

    added requirements.txt file

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 HERE = Path(__file__).parent
 README = (HERE / "README.md").read_text(encoding="utf8")
