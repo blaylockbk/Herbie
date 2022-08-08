eccffe1d2a309e3c2ce316e79c6ea8c1686ad5c0
Coat <alain.coat@actimar.fr>
2022-08-08 18:26:48 +0200

    add gdas

--- herbie/models/gfs.py
+++ herbie/models/gfs.py
@@ -2,6 +2,35 @@
 ## July 26, 2021
 
 
+class gdas:
+    def template(self):
+        self.DESCRIPTION = "Global Data Assimilation System"
+        self.DETAILS = {
+            "nomads product description": "https://www.nco.ncep.noaa.gov/pmb/products/gfs",
+            "google cloud platform": "https://console.cloud.google.com/marketplace/product/noaa-public/gfs?q=search&referrer=search&project=python-232920",
+            "azure document": "https://github.com/microsoft/AIforEarthDatasets#noaa-global-forecast-system-gfs",
+        }
+        self.PRODUCTS = {
+            "pgrb2.0p25": "common fields, 0.25 degree resolution",
+            "pgrb2.0p50": "common fields, 0.50 degree resolution",
+            "pgrb2.1p00": "common fields, 1.00 degree resolution",
+            "pgrb2b.0p25": "uncommon fields, 0.25 degree resolution",
+            "pgrb2b.0p50": "uncommon fields, 0.50 degree resolution",
+            "pgrb2b.1p00": "uncommon fields, 1.00 degree resolution",
+            "pgrb2full.0p50": "combined grids of 0.50 resolution",
+        }
+        self.SOURCES = {
+            "aws": f"https://noaa-gfs-bdp-pds.s3.amazonaws.com/gdas.{self.date:%Y%m%d/%H}/atmos/gdas.t{self.date:%H}z.{self.product}.f{self.fxx:03d}",
+            "aws-old": f"https://noaa-gfs-bdp-pds.s3.amazonaws.com/gdas.{self.date:%Y%m%d/%H}/gdas.t{self.date:%H}z.{self.product}.f{self.fxx:03d}",
+            "ftpprd": f"https://ftpprd.ncep.noaa.gov/data/nccf/com/gfs/prod/gdas.{self.date:%Y%m%d/%H}/atmos/gdas.t{self.date:%H}z.{self.product}.f{self.fxx:03d}",
+            "nomads": f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gdas.{self.date:%Y%m%d/%H}/atmos/gdas.t{self.date:%H}z.{self.product}.f{self.fxx:03d}",
+            "google": f"https://storage.googleapis.com/global-forecast-system/gdas.{self.date:%Y%m%d/%H}/atmos/gdas.t{self.date:%H}z.{self.product}.f{self.fxx:03d}",
+            "azure": f"https://noaagfs.blob.core.windows.net/gfs/gdas.{self.date:%Y%m%d/%H}/atmos/gfs.t{self.date:%H}z.{self.product}.f{self.fxx:03d}",
+        }
+        self.IDX_SUFFIX = [".idx"]
+        self.LOCALFILE = f"{self.get_remoteFileName}"
+
+
 class gfs:
     def template(self):
         self.DESCRIPTION = "Global Forecast System"
