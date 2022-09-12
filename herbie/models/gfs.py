7b36a35699639966fe204469a79670489b2e732e
Coat <alain.coat@actimar.fr>
2022-09-12 17:42:40 +0200

    update gdas template

--- herbie/models/gfs.py
+++ herbie/models/gfs.py
@@ -6,18 +6,14 @@ class gdas:
     def template(self):
         self.DESCRIPTION = "Global Data Assimilation System"
         self.DETAILS = {
-            "nomads product description": "https://www.nco.ncep.noaa.gov/pmb/products/gfs",
+            "nomads product description": "https://www.nco.ncep.noaa.gov/pmb/products/gfs/#GDAS",
             "google cloud platform": "https://console.cloud.google.com/marketplace/product/noaa-public/gfs?q=search&referrer=search&project=python-232920",
             "azure document": "https://github.com/microsoft/AIforEarthDatasets#noaa-global-forecast-system-gfs",
+            "aws document": "https://registry.opendata.aws/noaa-gfs-bdp-pds",
         }
         self.PRODUCTS = {
             "pgrb2.0p25": "common fields, 0.25 degree resolution",
-            "pgrb2.0p50": "common fields, 0.50 degree resolution",
             "pgrb2.1p00": "common fields, 1.00 degree resolution",
-            "pgrb2b.0p25": "uncommon fields, 0.25 degree resolution",
-            "pgrb2b.0p50": "uncommon fields, 0.50 degree resolution",
-            "pgrb2b.1p00": "uncommon fields, 1.00 degree resolution",
-            "pgrb2full.0p50": "combined grids of 0.50 resolution",
         }
         self.SOURCES = {
             "aws": f"https://noaa-gfs-bdp-pds.s3.amazonaws.com/gdas.{self.date:%Y%m%d/%H}/atmos/gdas.t{self.date:%H}z.{self.product}.f{self.fxx:03d}",
@@ -38,6 +34,7 @@ class gfs:
             "nomads product description": "https://www.nco.ncep.noaa.gov/pmb/products/gfs",
             "google cloud platform": "https://console.cloud.google.com/marketplace/product/noaa-public/gfs?q=search&referrer=search&project=python-232920",
             "azure document": "https://github.com/microsoft/AIforEarthDatasets#noaa-global-forecast-system-gfs",
+            "aws document": "https://registry.opendata.aws/noaa-gfs-bdp-pds",
         }
         self.PRODUCTS = {
             "pgrb2.0p25": "common fields, 0.25 degree resolution",
