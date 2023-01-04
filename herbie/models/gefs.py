a2df87de606ae5fd806a080df9c7f4d3c0854093
Brian Blaylock <blaylockbk@users.noreply.github.com>
2023-01-03 21:08:13 -0800

    initial progress on #132. Needs testing and refinement.

--- herbie/models/gefs.py
+++ herbie/models/gefs.py
@@ -2,23 +2,70 @@
 ## March 11, 2022
 
 """
-A Herbie template for the GEFS GRIB2 files (2000-2019).
+A Herbie template for the GEFS (2017-Present) and GEFS Reforecast (2000-2019)
+GRIB2 products.
 
-These grib files are organized different from other model types.
-The files are grouped into variables and clumped by forecast range.
-Thus, instead of a one file having all the variables for that lead time,
-a file has one variable for many lead times. This changes the way
-a user would use Herbie to access GEFS data. A user will need to supply
-the "variable". For getting specific grib messages, you will use the
-"searchString" argument to key in on the variable of interest. However,
-you will still need to give a value for "fxx" to tell Herbie which
-directory to look for. Yeah, it's a little different paradigm for Herbie,
-but we can work with it.
+
 """
 
-
 class gefs:
-    """Template for GEFS data
+    def template(self):
+        self.DESCRIPTION = "Global Ensemble Forecast System (GEFS)"
+        self.DETAILS = {
+            "Amazon Open Data": "https://registry.opendata.aws/noaa-gefs/",
+        }
+
+        self.PRODUCTS = {
+            "pgrb2ap5": "?? ~83 Most common variables",
+            "pgrb2bp5": "?? ~425 Least common variables",
+            "pgrb2sp25": "?",
+            "wave": "?? Wave products",
+            "chem/pgrb2ap25": "?",
+            "chem/pgrb2ap5": "?",
+        }
+
+
+        if self.product == "pgrb2ap5":
+            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/atmos/{self.product}/"
+            filename = f"geavg.t{self.date:%H}z.pgrb2a.0p50.f{self.fxx:03d}"
+        elif self.product == "pgrb2bp5":
+            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/atmos/{self.product}/"
+            filename = f"gec00.t{self.date:%H}z.pgrb2b.0p50.f{self.fxx:03d}"
+        elif self.product == "pgrb2sp25":
+            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/atmos/{self.product}/"
+            filename = f"geavg.t{self.date:%H}z.pgrb2s.0p50.f{self.fxx:03d}"
+        elif self.product == "wave":
+            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/wave/gridded/{self.product}/"
+            filename = f"gefs.wave.t{self.date:%H}z.c00.global.0p25.f{self.fxx:03d}.grib2"
+        elif self.product == "chem/pgrb2ap25":
+            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/chem/pgrb2ap25/"
+            filename = f"gefs.chem.t{self.date:%H}}z.a2d_0p25.f{self.fxx:03d}.grib2"
+        elif self.product == "chem/pgrb2ap5":
+            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/chem/pgrb2ap5/"
+            filename = f"gefs.chem.t{self.date:%H}}z.a3d_0p50.f{self.fxx:03d}.grib2"
+
+        self.SOURCES = {
+            "aws": f"{fileroot}}{filename}"
+        }
+
+
+        self.IDX_SUFFIX = [".grb2.idx", ".grib2.idx"]
+        self.LOCALFILE = f"{self.get_remoteFileName}"
+
+
+class gefs_reforecast:
+    """Template for GEFS Reforecast data
+
+    These grib files are organized different from other model types.
+    The files are grouped into variables and clumped by forecast range.
+    Thus, instead of a one file having all the variables for that lead time,
+    a file has one variable for many lead times. This changes the way
+    a user would use Herbie to access GEFS data. A user will need to supply
+    the "variable". For getting specific grib messages, you will use the
+    "searchString" argument to key in on the variable of interest. However,
+    you will still need to give a value for "fxx" to tell Herbie which
+    directory to look for. Yeah, it's a little different paradigm for Herbie,
+    but we can work with it.
 
     Provide arguments for
 
