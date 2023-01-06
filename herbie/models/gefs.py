33e49396cf23124f64523b1862f86c6a8865836e
Brian Blaylock <blaylockbk@gmail.com>
2023-01-05 22:09:03 -0800

    added model template for GEFS to address #132

--- herbie/models/gefs.py
+++ herbie/models/gefs.py
@@ -8,6 +8,7 @@ GRIB2 products.
 
 """
 
+
 class gefs:
     def template(self):
         self.DESCRIPTION = "Global Ensemble Forecast System (GEFS)"
@@ -17,40 +18,67 @@ class gefs:
         }
 
         self.PRODUCTS = {
-            "pgrb2ap5": "?? ~83 Most common variables",
-            "pgrb2bp5": "?? ~425 Least common variables",
-            "pgrb2sp25": "?",
-            "wave": "?? Wave products",
-            "chem/pgrb2ap25": "?",
-            "chem/pgrb2ap5": "?",
+            "atmos.5": "Half degree atmos PRIMARY fields (pgrb2ap5); ~83 most common variables.",
+            "atmos.5b": "Half degree atmos SECONDARY fields (pgrb2bp5); ~500 least common variables",
+            "atmos.25": "Quarter degree atmos PRIMARY fields (pgrb2sp25); ~35 most common variables",
+            "wave": "Global wave products.",
+            "chem.5": "Chemistry fields on 0.5 degree grid",
+            "chem.25": "Chemistry fields on 0.25 degree grid",
         }
 
+        if self.product == "wave":
+            if self.member == "spr":
+                self.member = "spread"
+            elif self.member == "avg":
+                self.member = "mean"
+        elif self.product.startswith("atmos"):
+            if self.member == "spread":
+                self.member = "spr"
+            elif self.member == "mean":
+                self.member = "avg"
 
-        if self.product == "pgrb2ap5":
-            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/atmos/{self.product}/"
-            filename = f"geavg.t{self.date:%H}z.pgrb2a.0p50.f{self.fxx:03d}"
-        elif self.product == "pgrb2bp5":
-            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/atmos/{self.product}/"
-            filename = f"gec00.t{self.date:%H}z.pgrb2b.0p50.f{self.fxx:03d}"
-        elif self.product == "pgrb2sp25":
-            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/atmos/{self.product}/"
-            filename = f"geavg.t{self.date:%H}z.pgrb2s.0p50.f{self.fxx:03d}"
-        elif self.product == "wave":
-            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/wave/gridded/{self.product}/"
-            filename = f"gefs.wave.t{self.date:%H}z.c00.global.0p25.f{self.fxx:03d}.grib2"
-        elif self.product == "chem/pgrb2ap25":
-            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/chem/pgrb2ap25/"
-            filename = f"gefs.chem.t{self.date:%H}}z.a2d_0p25.f{self.fxx:03d}.grib2"
-        elif self.product == "chem/pgrb2ap5":
-            fileroot = f"https://noaa-gefs-pds.s3.amazonaws.com/gefs.{self.date:%Y%m%d/%H}/chem/pgrb2ap5/"
-            filename = f"gefs.chem.t{self.date:%H}}z.a3d_0p50.f{self.fxx:03d}.grib2"
+        if self.member == 0:
+            self.member = "c00"
+        elif isinstance(self.member, int):
+            self.member = f"p{self.member:02d}"
+
+        filedir = f"gefs.{self.date:%Y%m%d/%H}"
+        filepaths = {
+            "atmos.5": f"{filedir}/atmos/pgrb2ap5/ge{self.member}.t{self.date:%H}z.pgrb2a.0p50.f{self.fxx:03d}",
+            "atmos.5b": f"{filedir}/atmos/pgrb2bp5/ge{self.member}.t{self.date:%H}z.pgrb2b.0p50.f{self.fxx:03d}",
+            "atmos.25": f"{filedir}/atmos/pgrb2sp25/ge{self.member}.t{self.date:%H}z.pgrb2s.0p50.f{self.fxx:03d}",
+            "wave": f"{filedir}/wave/gridded/gefs.wave.t{self.date:%H}z.{self.member}.global.0p25.f{self.fxx:03d}.grib2",
+            "chem.5": f"{filedir}/chem/pgrb2ap25/gefs.chem.t{self.date:%H}z.a2d_0p25.f{self.fxx:03d}.grib2",
+            "chem.25": f"{filedir}/chem/pgrb2ap25/gefs.chem.t{self.date:%H}z.a2d_0p25.f{self.fxx:03d}.grib2",
+        }
+
+        valid_members = {
+            "atmos.5": [f"p{i:02d}" for i in range(1, 31)] + ["c00", "spr", "avg"],
+            "atmos.5b": [f"p{i:02d}" for i in range(1, 31)],
+            "atmos.25": [f"p{i:02d}" for i in range(1, 31)] + ["c00", "spr", "avg"],
+            "wave": [f"p{i:02d}" for i in range(1, 31)] + ["spread", "mean", "prob"],
+            "chem.5": None,
+            "chem.25": None,
+        }
+
+        filepath = filepaths.get(self.product)
+        if filepath is None:
+            raise ValueError(
+                f"product={self.product} not recognized. Must be one of {self.PRODUCTS.keys()}"
+            )
+
+        _member = valid_members.get(self.product)
+        if _member is not None and self.member not in _member:
+            raise ValueError(
+                f"For GEFS product {self.product}, member must be one of {_member}"
+            )
 
         self.SOURCES = {
-            "aws": f"{fileroot}}{filename}"
+            "aws": f"https://noaa-gefs-pds.s3.amazonaws.com/{filepath}",
+            "nomads": f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/{filepath}",
         }
 
-
-        self.IDX_SUFFIX = [".grb2.idx", ".grib2.idx"]
+        self.IDX_SUFFIX = [".idx", ".grb2.idx", ".grib2.idx"]
         self.LOCALFILE = f"{self.get_remoteFileName}"
 
 
