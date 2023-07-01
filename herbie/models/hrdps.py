a802afb064b7b1caa4cf540c7070373fd8796080
Brian Blaylock <blaylockbk@gmail.com>
2023-06-30 22:42:59 -0700

    updated hrdps template file and added notebook

--- herbie/models/hrdps.py
+++ herbie/models/hrdps.py
@@ -9,6 +9,34 @@ The HRDPS is Canada's 2.5 km deterministic model
 
 Description: https://eccc-msc.github.io/open-data/msc-data/nwp_hrdps/readme_hrdps-datamart_en/#data-location
 Data Source: https://dd.weather.gc.ca/model_hrdps/
+
+- Continental domain: https://dd.weather.gc.ca/model_hrdps/continental/{res}/{HH}/{hhh}/
+- North domain (experimental): https://dd.weather.gc.ca/model_hrdps/north/grib2/{HH}/{hhh}/
+
+where `HH` is the models initialization time and `hhh` is the forecast lead time.
+
+Data available for last 24 hours.
+
+Other Levels
+------------
+`variable` is one of {'ALBDO', 'CAPE', 'CWAT', 'DEN', 'DEPR', 'DLWRF', 'DPT', 'DSWRF',
+       'GUST-Max', 'GUST-Min', 'GUST', 'HGT', 'HLCY', 'HPBL', 'ICEC',
+       'LAND', 'LHTFL', 'NLWRS', 'NSWRS', 'PRES', 'PRMSL', 'RH', 'SDEN',
+       'SDWE', 'SFCWRO', 'SHTFL', 'SKINT', 'SNOD', 'SOILVIC', 'SOILW',
+       'SPFH', 'TCDC', 'TMP', 'TSOIL', 'UGRD', 'ULWRF', 'USWRF', 'UTCI',
+       'VGRD', 'WDIR', 'WIND', 'WTMP'}
+`level` is one of {'Sfc', 'EATM', 'AGL-120m', 'AGL-40m', 'AGL-80m', 'AGL-2m', 'NTAT',
+       'AGL-10m', 'ISBY-1000-500', 'MSL', 'DBS-0-10cm', 'DBS-0-1cm'}
+
+Isobaric Levels
+---------------
+`variable` is one of {'ABSV', 'DEPR', 'HGT', 'MU-VT-LI', 'RH', 'SHWINX', 'SPFH', 'TMP',
+       'UGRD', 'VGRD', 'VVEL', 'WDIR', 'WIND'}
+`level` is "ISBL_####" where #### is one of
+    {'0250', '0500', '0700', '0850', '1000', '0050', '0100', '0150',
+    '0175', '0200', '0225', '0275', '0300', '0350', '0400', '0450',
+    '0550', '0600', '0650', '0750', '0800', '0875', '0900', '0925',
+'0950', '0970', '0985', '1015'}
 """
 
 
@@ -21,12 +49,16 @@ class hrdps:
             "Datamart product description": "https://eccc-msc.github.io/open-data/msc-data/nwp_hrdps/readme_hrdps-datamart_en/#data-location",
         }
         self.PRODUCTS = {
-            "continental": "",
+            "continental/2.5km": "continental domain",
         }
-        PATH = f"2.5km/{self.date:%H}/{self.fxx:03d}/{self.date:%Y%m%dT%HZ}_MSC_HRDPS-WEonG_{self.variable}_{self.level}_RLatLon0.0225_PT{self.fxx:03d}H.grib2"
+        PATH = f"{self.date:%H}/{self.fxx:03d}/{self.date:%Y%m%dT%HZ}_MSC_HRDPS_{self.variable}_{self.level}_RLatLon0.0225_PT{self.fxx:03d}H.grib2"
         self.SOURCES = {
             "msc": f"https://dd.weather.gc.ca/model_hrdps/{self.product}/{PATH}",
         }
 
         self.IDX_SUFFIX = [".grb2.idx", ".idx", ".grib.idx"]
         self.LOCALFILE = f"{self.get_remoteFileName}"
+
+
+class hrdps_north:
+    raise NotImplementedError("the hrdps_north product is not implemented yet")
