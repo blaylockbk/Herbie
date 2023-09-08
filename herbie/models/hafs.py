aec95e27bc7e8cc70fe5081702e0e0559fb27ac8
Brian Blaylock <blaylockbk@users.noreply.github.com>
2023-09-08 08:25:14 -0700

    update comment

--- herbie/models/hafs.py
+++ herbie/models/hafs.py
@@ -4,14 +4,14 @@ A Herbie template for the HAFS model.
 
 # maybe use functool caching decorator here??
 def get_name_mapping(DATE):
-    """Get a mapping of the available hurricane names and labels."""
+    """Get a mapping of the available hurricane names and storm id."""
     # read some file/files at NOMADS
     # parse it into a dict with name:label
     pass
 
 class hafsa:
     def template(self):
-        self.DESCRIPTION = "Hurricane Analysis and Forecast System"
+        self.DESCRIPTION = "Hurricane Analysis and Forecast System (HAFS-A) with GFDL microphysics."
         self.DETAILS = {
             "Homepage": "https://wpo.noaa.gov/the-hurricane-analysis-and-forecast-system-hafs/",
             "Hurricane Forecast Improvement Program": "https://hfip.org/hafs",
@@ -24,10 +24,10 @@ class hafsa:
             "ww3": "?",
         }
 
-        # self.name is the hurricane label identifier (e.g., 11e)
+        # `self.name` is the hurricane storm identifier (e.g., 11e)
         # I want to get a list of labels for the requested datetime
-        # and produce a maping of the labels to the hurricane name
-        # so that if name='jova' it will get the hurricane name='11e'
+        # and produce a maping of the stormIDs to the hurricane name
+        # so that if name='jova' it will get the stormID name='11e'
 
         # TODO: If name is a regular hurricane name and not an ID label,
         # then get the ID from the name_mapping
@@ -41,7 +41,7 @@ class hafsa:
 
 class hafsb:
     def template(self):
-        self.DESCRIPTION = "Hurricane Analysis and Forecast System"
+        self.DESCRIPTION = "Hurricane Analysis and Forecast System (HAFS-B) Thompson microphysics and tc-pbl option."
         self.DETAILS = {
             "Homepage": "https://wpo.noaa.gov/the-hurricane-analysis-and-forecast-system-hafs/",
             "Hurricane Forecast Improvement Program": "https://hfip.org/hafs",
