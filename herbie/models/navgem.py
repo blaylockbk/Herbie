472d7fc4b54ffcd8b49e7d61c2846e9cd80b359a
Brian Blaylock <blaylockbk@gmail.com>
2024-01-19 18:54:26 +0000

    updated NAVGEM access example

--- herbie/models/navgem.py
+++ herbie/models/navgem.py
@@ -1,9 +1,9 @@
 ## Added by Brian Blaylock
 ## July 28, 2021
-__all__ = ["navgem"]
 
 
-class navgem:
+class navgem_nomads:
+    """NAVGEM on NOMADS."""
     def template(self):
         self.DESCRIPTION = "Navy Global Environment Model"
         self.DETAILS = {
@@ -18,9 +18,12 @@ class navgem:
         self.LOCALFILE = f"{self.get_remoteFileName}"
 
 
-# ! DOES NOT WORK
 class navgem_godae:
-    """Not great implementation, just playing around..."""
+    """
+    NAVGEM on GODAE.
+
+    Not great implementation.
+    """
 
     def template(self):
         self.DESCRIPTION = "Navy Global Environment Model"
@@ -33,7 +36,7 @@ class navgem_godae:
             "GCOM": "",
         }
         self.SOURCES = {
-            "g1": "https://usgodae.org/ftp/outgoing/fnmoc/models/navgem_0.5/2022/2022010306/US058GMET-GR1mdl.0018_0056_00600F0OF2022010306_0100_003500-000000air_temp",
+            #"g1": "https://usgodae.org/ftp/outgoing/fnmoc/models/navgem_0.5/2022/2022010306/US058GMET-GR1mdl.0018_0056_00600F0OF2022010306_0100_003500-000000air_temp",
             "godae": f"https://usgodae.org/ftp/outgoing/fnmoc/models/navgem_0.5/{self.date:%Y/%Y%m%d%H}/US058{self.product}-GR1mdl.0018_0056_{self.fxx:03d}00F0OF{self.date:%Y%m%d%H}_{self.level}{self.variable}",
         }
         self.LOCALFILE = f"{self.get_remoteFileName}"
