d9389ad75bf64ace59dee01621f521e12453d7ea
Coat <alain.coat@actimar.fr>
2024-03-01 12:27:22 +0100

    update ecmwf from @blaylockbk

--- tests/test_ecmwf.py
+++ tests/test_ecmwf.py
@@ -1,9 +1,5 @@
-## Brian Blaylock
-## October 13, 2021
+"""Tests for downloading ECMWF model."""
 
-"""
-Tests for downloading ECMWF model
-"""
 from datetime import datetime, timedelta
 
 from herbie import Herbie
@@ -14,11 +10,32 @@ today_str = yesterday.strftime("%Y-%m-%d %H:%M")
 save_dir = "$TMPDIR/Herbie-Tests/"
 
 
-def test_ecmwf():
+def test_ifs():
     H = Herbie(
         yesterday,
-        model="ecmwf",
+        model="ifs",
         product="oper",
+        save_dir=save_dir,
+    )
+
+    # Test full file download
+    H.download()
+    assert H.get_localFilePath().exists()
+
+    # Test partial file download
+    # temperature at all levels
+    H.download(":t:")
+    assert H.get_localFilePath(":t:").exists()
+
+    # Test partial file xarray
+    H.xarray(":10(?:u|v):", remove_grib=False)
+    assert H.get_localFilePath(":10(?:u|v):").exists()
+
+
+def test_aifs():
+    H = Herbie(
+        yesterday,
+        model="aifs",
         save_dir=save_dir,
     )
 
