a7d8b17f8c754dab09faa94f14fe86f796e29448
Brian Blaylock <blaylockbk@gmail.com>
2024-02-29 21:37:38 -0800

    initial attempt at updating ecmwf to ifs and aifs

--- herbie/models/ecmwf.py
+++ herbie/models/ecmwf.py
@@ -1,6 +1,3 @@
-## Added by Brian Blaylock
-## January 26, 2022
-
 """
 A Herbie template for the ECMWF Open Data GRIB2 files.
 
@@ -11,20 +8,25 @@ See the `media release <https://www.ecmwf.int/en/about/media-centre/news/2022/ec
 - License Statement: This data is published under a Creative Commons Attribution 4.0 International (CC BY 4.0). https://creativecommons.org/licenses/by/4.0/
 - Disclaimer: ECMWF does not accept any liability whatsoever for any error or omission in the data, their availability, or for any loss or damage arising from their use.
 
-https://confluence.ecmwf.int/display/DAC/ECMWF+open+data%3A+real-time+forecasts
+https://confluence.ecmwf.int/display/DAC/ECMWF+open+data%3A+real-time+forecasts+from+IFS+and+AIFS
+
+Update 2024-02-29: ECMWF changes from 0.4 degree resolution to 0.25 plus adds artificial IFS
+https://www.ecmwf.int/en/about/media-centre/news/2024/ecmwf-releases-much-larger-open-dataset
 
 """
 
 
-class ecmwf:
+class ifs:
     def template(self):
-        # TODO: This will need to be updated someday
-        model = "ifs"
-        resol = "0p25"
+        # Allow a user to select the deprecated 0p4-beta resolution,
+        # but default to the 0p25 product if resolution is not specified.
+        # Sounds like the 0p4-beta product will be deprecated in May 2024.
+        if self.resolution is None:
+            self.resolution = "0p25"
 
-        self.DESCRIPTION = "ECMWF open data"
+        self.DESCRIPTION = "ECMWF Open Data - Integrated Forecast System"
         self.DETAILS = {
-            "ECMWF": "https://confluence.ecmwf.int/display/DAC/ECMWF+open+data%3A+real-time+forecasts",
+            "ECMWF": "https://confluence.ecmwf.int/display/DAC/ECMWF+open+data%3A+real-time+forecasts+from+IFS+and+AIFS",
         }
         self.PRODUCTS = {
             "oper": "operational high-resolution forecast, atmospheric fields",
@@ -46,7 +48,7 @@ class ecmwf:
             product_suffix = "fc"
 
         post_root = (
-            f"{self.date:%Y%m%d/%Hz}/{model}/{resol}/{self.product}"
+            f"{self.date:%Y%m%d/%Hz}/ifs/{self.resolution}/{self.product}"
             f"/{self.date:%Y%m%d%H%M%S}-{self.fxx}h-{self.product}-{product_suffix}.grib2"
         )
 
@@ -55,6 +57,38 @@ class ecmwf:
             "azure": f"https://ai4edataeuwest.blob.core.windows.net/ecmwf/{post_root}",
             "azure-scda": f"https://ai4edataeuwest.blob.core.windows.net/ecmwf/{post_root.replace('oper', 'scda')}",
             "azure-waef": f"https://ai4edataeuwest.blob.core.windows.net/ecmwf/{post_root.replace('wave', 'waef')}",
+            "aws": f"https://ecmwf-forecasts.s3.eu-central-1.amazonaws.com/{post_root}",
+            "ecmwf": f"https://data.ecmwf.int/forecasts/{post_root}",
+        }
+        self.IDX_SUFFIX = [".index"]
+        self.IDX_STYLE = "eccodes"  # 'wgrib2' or 'eccodes'
+        self.LOCALFILE = f"{self.get_remoteFileName}"
+
+
+class aifs:
+    def template(self):
+        self.DESCRIPTION = (
+            "ECMWF Open Data - Artificial Inteligence Integrated Forecast System"
+        )
+        self.DETAILS = {
+            "ECMWF": "https://confluence.ecmwf.int/display/DAC/ECMWF+open+data%3A+real-time+forecasts+from+IFS+and+AIFS",
+        }
+        self.PRODUCTS = {
+            "oper": "operational high-resolution forecast, atmospheric fields",
+        }
+
+        # example file
+        # https://data.ecmwf.int/forecasts/20240229/00z/ifs/0p25/oper/20240229000000-0h-oper-fc.grib2
+
+        product_suffix = "fc"
+
+        post_root = (
+            f"{self.date:%Y%m%d/%Hz}/ifs/0p25/{self.product}"
+            f"/{self.date:%Y%m%d%H%M%S}-{self.fxx}h-{self.product}-{product_suffix}.grib2"
+        )
+
+        self.SOURCES = {
+            "azure": f"https://ai4edataeuwest.blob.core.windows.net/ecmwf/{post_root}",
             "aws": f"https://ecmwf-forecasts.s3.eu-central-1.amazonaws.com/{post_root}",
             "ecmwf": f"https://data.ecmwf.int/forecasts/{post_root}",
         }
