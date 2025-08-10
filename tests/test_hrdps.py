27d0cbf663da8d2ba065d8c0a342ea46ed330fad
Nikhil Shankar <nshankar@umich.edu>
2025-08-10 15:55:11 -0400

    add tests

---
+++ tests/test_hrdps.py
@@ -0,0 +1,69 @@
+## Nikhil Shankar
+## August 10, 2025
+
+"""Tests for downloading HRDPS model."""
+
+from datetime import datetime
+
+import pandas as pd
+
+from herbie import Herbie, config
+
+now = datetime.now()
+latest = pd.Timestamp("now").floor("6h") - pd.Timedelta("6h")
+
+save_dir = config["default"]["save_dir"] / "Herbie-Tests-Data/"
+
+# Remove all previous test data
+for i in (save_dir / "hrdps").rglob("*"):
+    if i.is_file():
+        i.unlink()
+
+
+def test_hrdps_download():
+    H = Herbie(
+        latest,
+        model="hrdps",
+        product="continental/2.5km",
+        variable="TMP",
+        level="AGL-2m",
+        overwrite=True,
+        save_dir=save_dir,
+    )
+    f = H.download()
+    assert H.get_localFilePath().exists()
+    f.unlink()
+
+
+def test_hrdps_xarray():
+    H = Herbie(
+        latest,
+        model="hrdps",
+        product="continental/2.5km",
+        variable="TMP",
+        level="AGL-2m",
+        overwrite=True,
+        save_dir=save_dir,
+    )
+    H.xarray(remove_grib=False)
+    assert H.get_localFilePath().exists()
+    H.get_localFilePath().unlink()
+
+
+def test_hrdps_to_netcdf():
+    """Check that a xarray Dataset can be written to a NetCDF file.
+
+    It is important that I have haven't put any python objects in the
+    xarray Dataset attributes.
+    """
+    H = Herbie(
+        latest,
+        model="hrdps",
+        product="continental/2.5km",
+        variable="TMP",
+        level="AGL-2m",
+        overwrite=True,
+        save_dir=save_dir,
+    )
+    ds = H.xarray(remove_grib=False)
+    ds.to_netcdf(save_dir / "test_hrdps_to_netcdf.nc")
