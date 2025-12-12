116ed73ffb23507a6788a2182c90de63bddc8a04
Brian Blaylock <blaylockbk@gmail.com>
2025-12-11 21:31:24 -0800

    refactor xarray method into its own file

--- src/herbie/experimental/core.py
+++ src/herbie/experimental/core.py
@@ -107,40 +107,8 @@ class NewHerbie(Herbie):
         backend_kwargs: dict = {},
     ):
         """Load data into xarray."""
-        # TODO: Refactor this into its own file xarray.py
-        # TODO: Better names for nodes in DataTree
-        import cfgrib
-        import xarray as xr
-
-        # Backend kwargs for cfgrib
-        backend_kwargs.setdefault("indexpath", "")
-        backend_kwargs.setdefault(
-            "read_keys",
-            [
-                "parameterName",
-                "parameterUnits",
-                "stepRange",
-                "uvRelativeToGrid",
-                "shapeOfTheEarth",
-                "orientationOfTheGridInDegrees",
-                "southPoleOnProjectionPlane",
-                "LaDInDegrees",
-                "LoVInDegrees",
-                "Latin1InDegrees",
-                "Latin2InDegrees",
-            ],
-        )
-        backend_kwargs.setdefault("errors", "raise")
+        from .xarray import load_grib2_into_xarray
 
         local_file = self.download(filters)
 
-        Hxr = cfgrib.open_datasets(
-            local_file,
-            backend_kwargs=backend_kwargs,
-            decode_timedelta=True,
-        )
-
-        if len(Hxr) == 1:
-            return Hxr[0]
-        else:
-            return xr.DataTree.from_dict({f"node{i}": ds for i, ds in enumerate(Hxr)})
+        return load_grib2_into_xarray(local_file, backend_kwargs=backend_kwargs)
