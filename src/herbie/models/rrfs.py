2b3e6de4901204e936e2e27e30271207dfee528b
Will Hobbs <45701090+williamhobbs@users.noreply.github.com>
2026-09-22 16:59:12 -0500

    remove prototype, rrfs_old

--- src/herbie/models/rrfs.py
+++ src/herbie/models/rrfs.py
@@ -93,29 +93,3 @@ class rrfs:
             }
 
         self.LOCALFILE = f"{self.get_remoteFileName}"
-
-# prototype version -
-class rrfs_old:
-    def template(self):
-        self.DESCRIPTION = "Rapid Refresh Forecast System (RRFS) (prototype)"
-        self.DETAILS = {
-            "aws product description": "https://registry.opendata.aws/noaa-rrfs/",
-        }
-        self.PRODUCTS = {
-            # Below are ensemble products found in ensprod/
-            "mean": "ensemble mean",
-            "avrg": "ensemble products: ???",
-            "eas": "ensemble products: ???",
-            "ffri": "ensemble products: ???",
-            "lpmm": "ensemble products: ???",
-            "pmmn": "ensemble products: ???",
-            "prob": "ensemble products: ???",
-            # Below are member products found in mem##/
-            "testbed.conus": "surface grids (one for each member)",
-            "na": "native grids (one for each member)",
-        }
-        self.SOURCES = {
-            "aws": f"https://noaa-rrfs-pds.s3.amazonaws.com/rrfs.{self.date:%Y%m%d/%H}/ensprod/rrfsce.t{self.date:%H}z.conus.{self.product}.f{self.fxx:02d}.grib2",
-            "aws-mem": f"https://noaa-rrfs-pds.s3.amazonaws.com/rrfs.{self.date:%Y%m%d/%H}/mem{self.member:02d}/rrfs.t{self.date:%H}z.mem{self.member:02d}.{self.product}f{self.fxx:03d}.grib2",
-        }
-        self.LOCALFILE = f"mem{self.member:02d}/{self.get_remoteFileName}"
